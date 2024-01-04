d341dcb68b4786a1489dd8da76f28e7e528042e6
Reiterovich <reiterovich.andriy@gmail.com>
2024-01-04 11:07:44 +0200

    fix bags

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,16 @@
 import { NavLink, Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
-import Home from 'pages/Home';
-import Movies from '../../pages/Movies';
-import FilmItem from '../../pages/FilmItem/filmItem';
+import { lazy, Suspense } from 'react';
+// import Home from 'pages/Home';
+// import Movies from '../../pages/Movies';
+// import FilmItem from '../../pages/FilmItem/filmItem';
 
 import { Container, List, ListItem } from './App.styled.components';
 
 const Cast = lazy(() => import('../Cast'));
 const Reviwes = lazy(() => import('../Reviwes'));
+const Home = lazy(() => import('../../pages/Home'));
+const Movies = lazy(() => import('../../pages/Movies'));
+const FilmItem = lazy(() => import('../../pages/FilmItem/filmItem'));
 
 const App = () => {
   return (
@@ -22,14 +25,16 @@ const App = () => {
           </ListItem>
         </List>
       </Container>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:kinoID" element={<FilmItem />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="rewievs" element={<Reviwes />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:kinoID" element={<FilmItem />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="rewievs" element={<Reviwes />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
